649d7298f2704c898f6609bfe8861fb2a5af7251
eadehemingway <eade.hemingway@gmail.com>
2019-11-16 23:50:06 +0000

    delete css file

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,9 @@
 import React, { Component } from 'react'
-import './App.css'
 import { TamponList } from './components/TamponList'
 
 class App extends Component {
   render() {
-    return (
-      <div className="App">
-        <TamponList />
-      </div>
-    )
+    return <TamponList />
   }
 }
 
