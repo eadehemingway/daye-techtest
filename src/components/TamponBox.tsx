e0d939347d0a6b9ad8fba49e1d479115f07b1662
eadehemingway <eade.hemingway@gmail.com>
2019-11-16 22:14:47 +0000

    turn tamponbox into functional component

--- src/components/TamponBox.tsx
+++ src/components/TamponBox.tsx
@@ -1,4 +1,4 @@
-import React, { Component } from 'react'
+import React from 'react'
 import { TamponMatrix } from './TamponMatrix'
 
 export type TamponBoxType = {
@@ -26,30 +26,28 @@ export type Tampon = {
   amount: number
 }
 
-export class TamponBox extends Component<TamponBoxProps> {
-  render() {
-    const { data, boxIndex } = this.props
-    let tamponInfo = data.tampon
+export const TamponBox = (props: TamponBoxProps) => {
+  const { data, boxIndex } = props
+  let tamponInfo = data.tampon
 
-    return (
-      <div className="tampon-box">
-        <img src={data.productImage} className="tampon-image"></img>
-        <div className="tampon-box-writing">
-          <p className="box-title">
-            <span className="box-title-size">{tamponInfo[0].size} size | </span>
-            <span className="box-title-price">
-              {data.price} {data.currency}
-            </span>
-          </p>
+  return (
+    <div className="tampon-box">
+      <img src={data.productImage} className="tampon-image"></img>
+      <div className="tampon-box-writing">
+        <p className="box-title">
+          <span className="box-title-size">{tamponInfo[0].size} size | </span>
+          <span className="box-title-price">
+            {data.price} {data.currency}
+          </span>
+        </p>
 
-          <div className="matrix-container">
-            {tamponInfo &&
-              tamponInfo.map((t: Tampon, i: number) => (
-                <TamponMatrix tampon={t} key={i} boxIndex={boxIndex} />
-              ))}
-          </div>
+        <div className="matrix-container">
+          {tamponInfo &&
+            tamponInfo.map((t: Tampon, i: number) => (
+              <TamponMatrix tampon={t} key={i} boxIndex={boxIndex} />
+            ))}
         </div>
       </div>
-    )
-  }
+    </div>
+  )
 }
